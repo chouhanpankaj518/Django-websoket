f6b42daaa4339e7575fe3889f5454599de2705b4
chouhanpankaj518 <chouhanpankaj518@gmail.com>
2025-09-11 19:39:52 +0530

    add the javascript code

--- App/consumer.py
+++ App/consumer.py
@@ -4,22 +4,27 @@ import json
 import asyncio
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.accept()  # accept the connection
-        print("socket connected ***")
-        # await self.send({
-        #      'type': 'text',
-        # })
+        await self.accept() 
+       
         await self.send(text_data="Connection established")
+        await self.channel_layer.group_add('chat_group',self.channel_name)
+        
+        print('channel layer...',self.channel_layer)
+        print('channel name...',self.channel_name)
         
     async def receive(self,text_data):
-        print("received message: ", text_data)
+        data = json.loads(text_data)
+        msg = data.get('msg', '')
+        print("Message from client:", msg)
+    
         for i in range(10):
-        #    await self.send({
-        #     'type': 'websocket.send',
-        #     'text': str(i)
-        #    })2
            await self.send(text_data = json.dumps({'count':i}))
            await asyncio.sleep(1)
         
+        
+        
     async def disconnect(self,event):
         print("socket disconnected **")
+        await self.channel_layer.group_discard('chat_group',self.channel_name)
+        print("channel layer",self.channel_layer)
+        print("channel name",self.channel_name)
